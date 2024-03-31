78add225d70050a495c70c0901fced113d262972
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-01 00:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function playAudio(audioName) {
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.src = `./sounds/${audioName}`;
+    audioPlayer.play();
+}
+
+function stopAudio() {
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
+
